b4a51d81bbf8c50593487f6b39167565617ee47a
sweet74a <31387700+sweet74a@users.noreply.github.com>
2017-08-27 20:45:18 +0300

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
-account_sid = "Your Account Sid"
-auth_token = "Your Auth Token"
-my_cell = "+Your Cell Number"
-my_twilio = "+Your Twilio Number"
+account_sid = "AC37b5699c2f2c158171b0d7b06881732b"
+auth_token = "c53431d10cb3c6f1f44194e76a495c18"
+my_cell = "++966567499854"
+my_twilio = "+19805532202"
